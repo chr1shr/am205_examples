0b0489b5b6002e0885c701a963cad514ff69bf7f
Chris Rycroft <chr@seas.harvard.edu>
2018-10-09 12:54:04 -0400

    Update euler.py to bring it in line with the slide notation.

--- 3_num_calculus/euler.py
+++ 3_num_calculus/euler.py
@@ -3,7 +3,7 @@
 from math import *
 
 # Initial variables and constants
-x=1
+y=1
 t=0
 dt=0.05
 lam=0.5
@@ -11,14 +11,14 @@ lam=0.5
 # Apply Euler step until t>2
 while t<=2:
 
-    # Analytic solution
-    xexact=exp(lam*t)
+    # Analytical solution
+    yexact=exp(lam*t)
 
     # Print the solutions and error
-    print t,x,xexact,x-xexact
+    print t,y,yexact,y-yexact
 
     # Euler step
-    x=x+dt*(lam*x)
+    y=y+dt*(lam*y)
 
     # Update time
     t+=dt
