cd22d0b2410758e36f2869f65821debf708e7ec9
Chris Rycroft <chr@seas.harvard.edu>
2019-11-09 00:38:39 -0500

    Comment updates to Lotka-Volterra code.

--- 3_num_calculus/l-v.py
+++ 3_num_calculus/l-v.py
@@ -10,17 +10,21 @@ beta=0.5
 gamma=0.4
 delta=0.4
 
-# Function
+# Function that evaluates the RHS of the ODE. It has two components,
+# representing the changes in prey and predator populations.
 def deriv(x,t):
     return np.array([alpha*x[0]-beta*x[0]*x[1],-gamma*x[1]+delta*x[0]*x[1]])
 
-# Solve ODE using the "odeint" library in SciPy
+# Specify the range of time values where the ODE should be solved at
 time=np.linspace(0,70,500)
 
-# Initial conditions, set to the initial
+# Initial conditions, set to the initial populations of prey and predators
 xinit=np.array([10,5])
+
+# Solve ODE using the "odeint" library in SciPy.
 x=odeint(deriv,xinit,time)
 
+# Print the solutions
 for i in range(0,500):
     print time[i],x[i,0],x[i,1]
 
