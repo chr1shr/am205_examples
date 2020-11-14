62097a8dee032a42a6f58b373e75d30524f55b09
Chris Rycroft <chr@seas.harvard.edu>
2020-11-14 12:23:46 -0500

    Overhaul Crank-Nicolson example.

--- 3_num_calculus/c-n.py
+++ 3_num_calculus/c-n.py
@@ -1,55 +1,54 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 import numpy as np
-from math import *
+from math import sin,exp,pi,sqrt
 
-# Grid size
-m=64
-a=np.zeros((m))
-b=np.zeros((m))
-c=np.zeros((m))
-iters=10000
+# Solves the heat equation using the Crank-Nicolson method using
+# a given number of grid points, and computes the L2 norm to a reference
+# solution
+def c_n(m):
+    a=np.empty((m+1))
+    b=np.empty((m+1))
 
-# PDE-related constants. The timestep is chosen to be just larger than the
-# point of stability, causing the 2-gridpoint oscillation to slowly grow.
-alpha=0.1
-dx=1.0/(m-1)
-dt=0.1*dx*dx/alpha
-nu=alpha*dt/(dx*dx)
+    # Set grid spacing, timestep, and mu
+    dx=1.0/m
+    dt=0.1/m
+    mu=dt/(dx*dx)
 
-# Matrix for Crank-Nicolson
-u=np.zeros((m,m))
-u[0,0]=1;u[m-1,m-1]=1
-for i in range(1,m-1):
-    u[i,i]=(1+nu)
-    u[i,i-1]=-0.5*nu
-    u[i,i+1]=-0.5*nu
+    # Set initial condition
+    for i in range(m+1):
+        a[i]=sin(i*pi*dx)
+    b[0]=0;b[m]=0;
 
-# Initial condition
-for i in range(1,m-1):
-    a[i]=sin(i*dx*pi)
-    b[i]=sin(i*dx*pi)
+    # Create linear system matrix for implicit step in Crank-Nicolson. Note
+    # that here the matrix is represented and solved using dense linear
+    # algebra, which is inefficient for large m. Using a dedicated tridiagonal
+    # solver would be more efficient.
+    A=np.diag(np.ones(m+1)*(1+mu))+ \
+      np.diag(np.ones(m)*(-0.5*mu),1)+ \
+      np.diag(np.ones(m)*(-0.5*mu),-1)
+    A[0,0]=1;A[0,1]=0
+    A[m,m]=1;A[m,m-1]=0
 
-# Integrate the PDE
-print(0.,0.,0.)
-for j in range(0,iters+1):
+    # Perform timesteps
+    for j in range(m):
+        for i in range(1,m):
+            b[i]=(1-mu)*a[i]+0.5*mu*(a[i-1]+a[i+1])
+        a=np.linalg.solve(A,b)
 
-    # Forward Euler
-    for i in range(1,m-1):
-        c[i]=(1-2*nu)*a[i]+nu*(a[i-1]+a[i+1])
-    a=np.copy(c)
+    # Evaluate L2 norm to reference solution
+    s=0;f=exp(-pi*pi*0.1)
+    for i in range(1,m):
+        du=a[i]-sin(i*pi*dx)*f
+        s+=du*du
+    return sqrt(dx*s)
 
-    # Crank-Nicolson
-    for i in range(1,m-1):
-        c[i]=(1-nu)*b[i]+0.5*nu*(b[i-1]+b[i+1])
-    c[0]=0;c[m-1]=0;
-    b=np.linalg.solve(u,c)
+# Loop over a range of grid sizes
+m=4
+while m<=512:
 
-    # L2 norm
-    if (j+1)%100==0:
-        t=(j+1)*dt
-        l2a=0;l2b=0
-        for i in range(1,m-1):
-            sol=sin(i*dx*pi)*exp(-alpha*pi*pi*t)
-            l2a+=(sol-a[i])**2
-            l2b+=(sol-b[i])**2
-        print(t,sqrt(l2a/m),sqrt(l2b/m))
+    # Print the number of grid points, the grid spacing, and the L2 norm
+    # for Crank-Nicolson
+    print(m,1.0/m,c_n(m))
+
+    # Double the number of grid points
+    m*=2
